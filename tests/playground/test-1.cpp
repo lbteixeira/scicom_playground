78ec02c8d60b158801899e1dc9f1195ce4de3ad1
Lucas Teixeira <lucas@lbteixeira.com>
2022-08-21 18:47:45 +0200

    including tests

---
+++ tests/playground/test-1.cpp
@@ -0,0 +1,34 @@
+#include <catch2/catch_test_macros.hpp>
+
+#include "playground/blaze.hpp"
+#include "playground/eigen.hpp"
+
+TEST_CASE("Test case 1", "[tag1]") {
+  SECTION("First section") {
+    REQUIRE(1 + 1 == 2);
+  }
+}
+
+TEST_CASE("Eigen") {
+  int size = 2;
+  int value = 1;
+  Eigen::MatrixXd m(size, size);
+
+  for (int a = 0; a < size; a++) {
+    for (int b = 0; b < size; b++) {
+      m(a, b) = value;
+    }
+  }
+
+  REQUIRE(m == populateMatrix(size, value));
+}
+
+TEST_CASE("Blaze") {
+  auto result = sumVectors();
+
+  const blaze::DynamicVector<int> expected{6, 3, 2};
+
+  REQUIRE(result[0] == expected[0]);
+  REQUIRE(result[1] == expected[1]);
+  REQUIRE(result[2] == expected[2]);
+}
